77afc7d61e26e4812af70e8ad262e605ede1db73
Bodil Stokke <bodil@bodil.org>
2015-02-17 23:24:49 +0000

    Slides for a talk.

---
+++ talk/slides.js
@@ -0,0 +1,7 @@
+var Pink = require("pink");
+require("pink/css/themes/simon.less");
+
+new Pink("#slides", {
+  "background": require("pink/modules/background"),
+  "image": require("pink/modules/image")
+});
